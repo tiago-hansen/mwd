da3916ece0e91aafc57fb2669f6640022a671cc7
tiago-hansen <tiagolorahansen@gmail.com>
2024-12-24 00:48:46 -0300

    Add Student model with fields: name, email, and registration

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    registration = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
