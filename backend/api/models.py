481bf75f44153ca6e50d840ffe45771d517d5f1d
tiago-hansen <tiagolorahansen@gmail.com>
2024-12-31 18:41:22 -0300

    Add models for UndergraduateProgram, Course, and Enrollment in Django

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class Student(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
@@ -10,3 +8,40 @@ class Student(models.Model):
     def __str__(self):
         return self.name
 
+
+class UndergraduateProgram(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.code
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=10)
+    undergraduate_program = models.ForeignKey(UndergraduateProgram, on_delete=models.CASCADE)
+    SEMESTER_CHOICES = [
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+    ]
+    semester = models.IntegerField(choices=SEMESTER_CHOICES)
+
+    def __str__(self):
+        return self.code
+
+
+class Enrollment(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    grade = models.FloatField()
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.student.registration} - {self.course.code} - {self.grade}'
